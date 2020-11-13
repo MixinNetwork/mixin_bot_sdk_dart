a0827ba810559d5c755a35d10642b135b69da07b
touge <touge1027@gmail.com>
2020-11-13 17:25:48 +0800

    Support sign authentication token with eddsa

--- lib/src/auth.dart
+++ lib/src/auth.dart
@@ -1,14 +1,27 @@
 import 'dart:convert';
 
 import 'package:convert/convert.dart';
+import 'package:crypto_keys/crypto_keys.dart';
 import 'package:cryptography/cryptography.dart';
 import 'package:jose/jose.dart';
 import 'package:uuid/uuid.dart';
 
 import './extension/string_extension.dart';
 
-String signAuthenticationToken(
+String signAuthTokenWithRSA(
     String userId, String sessionId, String privateKey, method, uri, body) {
+  return _signAuthenticationToken(
+      userId, sessionId, privateKey, method, uri, body, true);
+}
+
+String signAuthTokenWithEdDSA(
+    String userId, String sessionId, String privateKey, method, uri, body) {
+  return _signAuthenticationToken(
+      userId, sessionId, privateKey, method, uri, body, false);
+}
+
+String _signAuthenticationToken(String userId, String sessionId,
+    String privateKey, method, uri, body, bool isRSA) {
   if (userId.isNullOrEmpty ||
       sessionId.isNullOrEmpty ||
       privateKey.isNullOrEmpty) {
@@ -29,10 +42,20 @@ String signAuthenticationToken(
     'scp': 'FULL',
   });
 
-  final key = JsonWebKey.fromPem(privateKey);
+  JsonWebKey key;
+  String alg;
+  if (isRSA) {
+    key = JsonWebKey.fromPem(privateKey);
+    alg = 'RS512';
+  } else {
+    var privateBytes = base64.decode(privateKey);
+    key = JsonWebKey.fromCryptoKeys(
+        privateKey: EdDSAPrivateKey(bytes: privateBytes));
+    alg = 'EdDSA';
+  }
   final builder = JsonWebSignatureBuilder();
   builder.jsonContent = claims.toJson();
-  builder.addRecipient(key, algorithm: 'RS512');
+  builder.addRecipient(key, algorithm: alg);
 
   return '${builder.build().toCompactSerialization()}';
 }
