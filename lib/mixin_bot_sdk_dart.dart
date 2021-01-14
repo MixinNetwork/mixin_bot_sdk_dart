e202d11c329e9ae04de96f45b572ba6297e37996
YeungKC <I@YeungKC.com>
2021-01-14 23:19:56 +0800

    export participant_request.

--- lib/mixin_bot_sdk_dart.dart
+++ lib/mixin_bot_sdk_dart.dart
@@ -12,6 +12,7 @@ export 'src/vo/mixin_response.dart';
 export 'src/vo/user.dart';
 export 'src/vo/user_session.dart';
 export 'src/vo/provisioning.dart';
+export 'src/vo/participant_request.dart';
 export 'src/vo/request/provisioning_request.dart';
 export 'src/vo/blaze_ack_message.dart';
 export 'src/extension/mixin_response_extension.dart';
