7c77844edd34cf2bf532c30ca603474e5a472e1b
YeungKC <I@YeungKC.com>
2021-03-16 20:39:01 +0800

    Fix null safety.

---
+++ lib/src/vo/participant_request.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'participant_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ParticipantRequest _$ParticipantRequestFromJson(Map<String, dynamic> json) {
+  $checkKeys(json, disallowNullValues: const ['user_id']);
+  return ParticipantRequest(
+    userId: json['user_id'] as String,
+    role: _$enumDecode(_$ParticipantRoleEnumMap, json['role']),
+    createdAt: DateTime.parse(json['created_at'] as String),
+  );
+}
+
+Map<String, dynamic> _$ParticipantRequestToJson(ParticipantRequest instance) =>
+    <String, dynamic>{
+      'user_id': instance.userId,
+      'role': _$ParticipantRoleEnumMap[instance.role],
+      'created_at': instance.createdAt.toIso8601String(),
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$ParticipantRoleEnumMap = {
+  ParticipantRole.owner: 'owner',
+  ParticipantRole.admin: 'admin',
+};
