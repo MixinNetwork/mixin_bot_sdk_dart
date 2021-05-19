eecf57e905896f7c2a224ab04ff66311cb2469ba
YeungKC <I@YeungKC.com>
2021-05-19 17:17:44 +0800

    When request has a error, auto switch host and retry.

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -15,6 +15,9 @@ import 'auth.dart';
 import 'error/mixin_api_error.dart';
 import 'log_interceptor.dart';
 
+const mixinBaseUrl0 = 'https://api.mixin.one';
+const mixinBaseUrl1 = 'https://mixin-api.zeromesh.net';
+
 class Client {
   Client({
     String? userId,
@@ -28,7 +31,7 @@ class Client {
     HttpLogLevel? httpLogLevel = HttpLogLevel.all,
   }) {
     _dio = Dio(dioOptions);
-    _dio.options.baseUrl = baseUrl ?? 'https://api.mixin.one';
+    _dio.options.baseUrl = baseUrl ?? mixinBaseUrl0;
     _dio.options.responseType = ResponseType.json;
     (dio.transformer as DefaultTransformer).jsonDecodeCallback =
         jsonDecodeCallback;
@@ -67,6 +70,34 @@ class Client {
           ),
           true,
         );
+      },
+      onError: (DioError e, ErrorInterceptorHandler handler) async {
+        if ((e is MixinApiError && e.error.code < 500)) {
+          return handler.next(e);
+        }
+
+        dio.options.baseUrl =
+            dio.options.baseUrl == mixinBaseUrl0 ? mixinBaseUrl1 : mixinBaseUrl0;
+
+        try {
+          var response = await dio.request(
+            e.requestOptions.path,
+            data: e.requestOptions.data,
+            queryParameters: e.requestOptions.queryParameters,
+            cancelToken: e.requestOptions.cancelToken,
+            options: Options(
+              method: e.requestOptions.method,
+              headers: e.requestOptions.headers,
+              responseType: e.requestOptions.responseType,
+              contentType: e.requestOptions.contentType,
+            ),
+            onSendProgress: e.requestOptions.onSendProgress,
+            onReceiveProgress: e.requestOptions.onReceiveProgress,
+          );
+          return handler.resolve(response);
+        } catch (error) {
+          return handler.reject(error is DioError ? error : e);
+        }
       },
     ));
     if (httpLogLevel != null) {
